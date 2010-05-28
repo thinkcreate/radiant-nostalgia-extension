1a7f8be6e7705047c6763c4a12650941a603db76
Gert Goet <gert@thinkcreate.nl>
2010-05-29 01:12:13 +0200

    support regexp

--- app/models/not_found_with_redirects_page.rb
+++ app/models/not_found_with_redirects_page.rb
@@ -3,11 +3,18 @@ class NotFoundWithRedirectsPage < FileNotFoundPage
     false
   end
 
+  def mappings
+    # [['/old','/new'],...]
+    @mappings ||= begin
+      lines = render_part(:redirects).split(%{\r\n})
+      lines.inject([]){|res,line| res << line.split(/ +\=> +/, 2);res}
+    end
+  end
+  
   def process(request, response)
     super
     request_uri = request.request_uri
-    mapper = Nostalgia::Mapper.from_content(render_part(:redirects))
-    if location = mapper.match(request_uri)
+    if location = find_location(request_uri)
       @response.headers["Location"] = location
       @response.body = <<-HTML
 <html>
@@ -18,5 +25,26 @@ class NotFoundWithRedirectsPage < FileNotFoundPage
       HTML
       @response.status = 301
     end
+  end
+  
+  
+  def find_location(request_uri)
+    captures = nil
+    match = mappings.detect do |(from,to)|
+      captures = request_uri.scan(Regexp.new(from)).flatten
+      !captures.empty?
+    end
+    return nil unless match
+    to = match.last
+    build_path_from_captures(to, captures)
+    to
+  end
+  
+  protected
+  def build_path_from_captures(path, captures)
+    captures.each_with_index do |cap, ix|
+      path.gsub!(%{$#{ix+1}}, cap)
+    end
+    path
   end
 end
